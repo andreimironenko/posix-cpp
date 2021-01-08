526206dfc870111965b89b44b285409ade6e125d
Andrei Mironenko <andrei.mironenko@paralleldynamic.com>
2021-01-08 00:36:33 +0000

    Got first working version, but not cleaned and documented

---
+++ src/timer_.h
@@ -0,0 +1,45 @@
+#include <map>
+#include <memory>
+#include <ratio>
+#include <chrono>
+#include <ctime>
+
+#include <syslog.h>
+
+#include "timer.h"
+
+#pragma once
+
+  class timer::timer_
+  {
+    std::chrono::duration<long, std::nano> _period;
+    callback_t _callback;
+    void* _data;
+    bool _is_single_shot;
+    int _signal;
+
+    struct itimerspec _ts;
+    struct sigaction _sa;
+    struct sigevent _sev;
+
+    timer_t _timer;
+
+    public:
+    static void signal_handler(int sig, siginfo_t *si, void *uc = nullptr);
+
+    explicit timer_(std::chrono::duration<long, std::nano> period_nsec, callback_t callback, void* data,
+        bool is_single_short, int sig);
+
+    ~timer_();
+
+    timer_(const timer_&) = delete;
+    timer_(timer_&&) = delete;
+    timer_& operator=(const timer_&) = delete;
+    timer_& operator=(timer_&&) = delete;
+
+    void start();
+    void reset();
+    void suspend();
+    void resume();
+    void stop();
+  };
---
+++ tests/timer-test.cpp
@@ -0,0 +1,107 @@
+// Created by amironenko on 19/11/2020.
+//
+#include <chrono>
+#include <ratio>
+#include <memory>
+#include <functional>
+
+#include <gtest/gtest.h>
+
+#include "timer.h"
+
+using namespace std;
+using namespace chrono;
+
+class TimerTest: public ::testing::Test {
+  protected:
+
+  public:
+    int _tick = 0;
+
+    TimerTest()
+    {
+      // initialization;
+    }
+
+    void SetUp( ) override
+    {
+      // initialization or some code to run before each test
+    }
+
+    void TearDown( ) override
+    {
+      // code to run after each test;
+      // can be used instead of a destructor,
+      // but exceptions can be handled in this function only
+      _tick = 0;
+    }
+
+    void increment_tick(void* tick)
+    {
+      EXPECT_EQ((long)tick, (long)&_tick);
+      //_tick ++;
+      (*((int*)tick))++;
+    }
+
+    ~TimerTest( )  override {
+      // resources cleanup, no exceptions allowed
+    }
+};
+
+TEST_F(TimerTest, CreatingNew)
+{
+  EXPECT_TRUE(true);
+  duration<long> period_s(5s);
+
+  std::unique_ptr<timer> tm (
+      new timer(
+        period_s,
+        std::bind(&TimerTest::increment_tick, this, std::placeholders::_1), // callback
+       (void*) &_tick )                                                     // pointer to data
+      );
+  tm->start();
+
+  int max_ticks = 5;
+  for(int i = 0; i < max_ticks; i++)
+  {
+    sleep(period_s.count());
+    EXPECT_EQ(_tick, i+1);
+    cout << "tick: " << _tick << endl;
+  }
+
+  tm->stop();
+  sleep(period_s.count());
+
+  EXPECT_EQ(_tick, max_ticks);
+}
+
+TEST_F(TimerTest, SuspendResume)
+{
+  EXPECT_TRUE(true);
+  duration<long> period_s(5s);
+
+  std::unique_ptr<timer> tm (
+      new timer(
+        period_s,
+        std::bind(&TimerTest::increment_tick, this, std::placeholders::_1), // callback
+        (void*) &_tick,                                                     // pointer to data
+        true
+        )
+      );
+
+  tm->start();
+
+  sleep(1);
+
+  tm->suspend();
+
+  sleep(1);
+
+  tm->resume();
+
+  sleep(4);
+
+  EXPECT_EQ(_tick, 1);
+}
+
+
